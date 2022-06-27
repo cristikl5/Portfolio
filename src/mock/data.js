6dfd240a5474cb747c95ed58def46fd7ae65b685
cristiankl <cristian@bizequity.com>
2022-06-27 14:26:37 +0300

    enh(ui): added project

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,83 +1,93 @@
-import {nanoid} from 'nanoid';
+import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-    title: 'My Portfolio', // e.g: 'Name | Developer'
-    lang: '', // e.g: en, es, fr, jp
-    description: '', // e.g: Welcome to my website
+  title: 'My Portfolio', // e.g: 'Name | Developer'
+  lang: '', // e.g: en, es, fr, jp
+  description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-    title: '',
-    name: 'Calaras Cristian',
-    subtitle: 'Front End Developer',
-    cta: '',
+  title: '',
+  name: 'Calaras Cristian',
+  subtitle: 'Front End Developer',
+  cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-    img: 'photo3x42.jpg',
-    paragraphOne: 'My name is Calaras Cristian I\'m a  Front End Developer based in Chisinau.I describe myself as a passionate developer who loves coding, open source, and the web platform',
-    paragraphTwo: '',
-    paragraphThree: '',
-    resume: '',
+  img: 'photo3x42.jpg',
+  paragraphOne:
+    "My name is Calaras Cristian I'm a  Front End Developer based in Chisinau.I describe myself as a passionate developer who loves coding, open source, and the web platform",
+  paragraphTwo: '',
+  paragraphThree: '',
+  resume: '',
 };
 
 // PROJECTS DATA
 export const projectsData = [
-    {
-        id: nanoid(),
-        img: 'img_2.png',
-        title: 'React.js Expense Tracker',
-        info: 'An expense tracker is a software or application that helps to keep an accurate record of your money inflow and outflow',
-        info2: '',
-        url: 'https://expense-tracker5.herokuapp.com/',
-        repo: 'https://github.com/cristikl5/ExpenseTracker', // if no repo, the button will not show up
-    },
-    {
-        id: nanoid(),
-        img: 'img_1.png',
-        title: ' Angular Login&Register App with Firebase and REST Api',
-        info: 'Login&Register with Firebase and REST Api ',
-        info2: '',
-        url:'',
-        repo: 'https://github.com/cristikl5/Angular-CRM-App', // if no repo, the button will not show up
-    },
-    {
-        id: nanoid(),
-        img: 'img_3.png',
-        title: 'CleverGiver',
-        info: 'Angular Development',
-        info2: '',
-        url:'https://clevergiver.com/',
-    },
+  {
+    id: nanoid(),
+    img: 'img_2.png',
+    title: 'React.js Expense Tracker',
+    info:
+      'An expense tracker is a software or application that helps to keep an accurate record of your money inflow and outflow',
+    info2: '',
+    url: 'https://expense-tracker5.herokuapp.com/',
+    repo: 'https://github.com/cristikl5/ExpenseTracker', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'img_1.png',
+    title: ' Angular Login&Register App with Firebase and REST Api',
+    info: 'Login&Register with Firebase and REST Api ',
+    info2: '',
+    url: '',
+    repo: 'https://github.com/cristikl5/Angular-CRM-App', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'img_3.png',
+    title: 'CleverGiver',
+    info: 'Angular Development',
+    info2: '',
+    url: 'https://clevergiver.com/',
+  },
+  {
+    id: nanoid(),
+    img: 'bizleads-snapshot.png',
+    title: 'Bizleads',
+    info: 'Angular Development',
+    info2: '',
+    url: 'https://bizleads.com/',
+  },
 ];
 
 // CONTACT DATA
 export const contactData = {
-    cta: 'Phone:+37360913145',
-    btn: 'EMAIL',
-    email: 'cristian.calaras88@gmail.com',
+  cta: 'Phone:+37360913145',
+  btn: 'EMAIL',
+  email: 'cristian.calaras88@gmail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
-    networks: [
-        {
-            id: nanoid(),
-            name: 'linkedin',
-            url: 'https://linkedin.com/in/cristikl',
-        },
-        {
-            id: nanoid(),
-            name: 'github',
-            url: 'https://github.com/cristikl5',
-        },
-    ],
+  networks: [
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://linkedin.com/in/cristikl',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'https://github.com/cristikl5',
+    },
+  ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-    isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
 };
