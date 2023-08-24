b5bc805ca6e7bbfdd10f2cc46f9d587b81f81f04
Cristian <cristian@bizequity.com>
2023-08-24 14:24:17 +0300

    fix(ui): updated gatsby sass loader and made font size of points bigger

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -49,7 +49,7 @@ const Projects = () => {
                           {info ||
                             'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
                         </p>
-                        <ul style={{ fontSize: '1.2rem' }}>
+                        <ul style={{ fontSize: '1.5rem' }}>
                           {info2.map((point) => (
                             <li>{point}</li>
                           ))}
